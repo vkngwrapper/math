88bdb35bd8cd061536bd789ff48db960d4b1346d
Stephen Baynham <sdbaynham@gmail.com>
2025-12-14 15:09:17 -0800

    remove external math library deps, mod tidy, go 1.25

--- bench_test.go
+++ bench_test.go
@@ -1,85 +1,13 @@
 package math
 
 import (
-	"github.com/g3n/engine/math32"
-	"github.com/go-gl/mathgl/mgl32"
-	"github.com/ungerik/go3d/mat4"
-	"github.com/ungerik/go3d/quaternion"
-	"github.com/ungerik/go3d/vec3"
-	"github.com/ungerik/go3d/vec4"
 	"math"
 	"testing"
 )
 
-var g3nGlobalOutVec4 math32.Vector4
-var g3nGlobalOutVec3 math32.Vector3
-
-var mathglGlobalOutVec4 mgl32.Vec4
-var mathglGlobalOutVec3 mgl32.Vec3
-
-var go3dGlobalOutVec4 vec4.T
-var go3dGlobalOutVec3 vec3.T
-
 var vkngMathOutVec4 Vec4[float32]
 var vkngMathOutVec3 Vec3[float32]
 var vkngMathOutQuat Quaternion[float32]
-
-func BenchmarkTransformVec4G3n(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		transform := math32.Vector4{X: 5, Y: 10, Z: 15, W: 1}
-
-		var translateMat, rotateMat, scaleMat math32.Matrix4
-		translateMat.MakeTranslation(1, 1, 1)
-		rotateMat.MakeRotationY(1)
-		scaleMat.MakeScale(1.5, 1.5, 1.5)
-
-		var mat math32.Matrix4
-		mat.MultiplyMatrices(
-			&translateMat,
-			&rotateMat,
-		)
-		mat.Scale(&math32.Vector3{X: 1.5, Y: 1.5, Z: 1.5})
-
-		g3nGlobalOutVec4 = *(transform.ApplyMatrix4(&mat))
-	}
-}
-
-func BenchmarkTransformVec4MathGL(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-
-		mat := mgl32.Ident4().
-			Mul4(mgl32.Translate3D(1, 1, 1)).
-			Mul4(mgl32.HomogRotate3DY(1)).
-			Mul4(mgl32.Scale3D(1.5, 1.5, 1.5))
-		transform := mgl32.Vec4{5.0, 10.0, 15.0, 1.0}
-		transform = mat.Mul4x1(transform)
-
-		mathglGlobalOutVec4 = transform
-	}
-}
-
-func BenchmarkTransformVec4Go3D(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		translate := vec3.T{1, 1, 1}
-		scale := vec3.T{1.5, 1.5, 1.5}
-
-		rotate := mat4.Ident
-		rotate.AssignYRotation(1)
-
-		mat := mat4.Ident
-		mat.Translate(&translate)
-		mat.MultMatrix(&rotate)
-		mat.ScaleVec3(&scale)
-		transform := vec4.T{5, 10, 15, 1}
-		mat.TransformVec4(&transform)
-
-		go3dGlobalOutVec4 = transform
-	}
-}
 
 func BenchmarkTransformVec4VkngMath(b *testing.B) {
 	b.ReportAllocs()
@@ -98,42 +26,6 @@ func BenchmarkTransformVec4VkngMath(b *testing.B) {
 	}
 }
 
-func BenchmarkRotateVec3G3n(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		transform := math32.Vector3{X: 5, Y: 10, Z: 15}
-		axis := math32.Vector3{X: 1, Y: 0, Z: 0}
-
-		transform.ApplyAxisAngle(&axis, math.Pi)
-
-		g3nGlobalOutVec3 = transform
-	}
-}
-
-func BenchmarkRotateVec3MathGL(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := mgl32.Vec3{5, 10, 15}
-		axis := mgl32.Vec3{1, 0, 0}
-		quaternion := mgl32.QuatRotate(math.Pi, axis)
-
-		mathglGlobalOutVec3 = quaternion.Rotate(transform)
-	}
-}
-
-func BenchmarkRotateVec3Go3D(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := vec3.T{5, 10, 15}
-		quaternion := quaternion.FromXAxisAngle(math.Pi)
-
-		quaternion.RotateVec3(&transform)
-		go3dGlobalOutVec3 = transform
-	}
-}
-
 func BenchmarkRotateVec3VkngMath(b *testing.B) {
 	b.ReportAllocs()
 
@@ -143,47 +35,6 @@ func BenchmarkRotateVec3VkngMath(b *testing.B) {
 
 		transform.Rotate(math.Pi, &axis)
 		vkngMathOutVec3 = transform
-	}
-}
-
-func BenchmarkRotateQuaternionG3n(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := math32.Vector3{X: 5, Y: 10, Z: 15}
-		euler := math32.Vector3{X: math.Pi, Y: 0, Z: math.Pi / 2}
-
-		var quaternion math32.Quaternion
-
-		quaternion.SetFromEuler(&euler)
-
-		transform.ApplyQuaternion(&quaternion)
-
-		g3nGlobalOutVec3 = transform
-	}
-}
-
-func BenchmarkRotateQuaternionMathGL(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := mgl32.Vec3{5, 10, 15}
-
-		quaternion := mgl32.AnglesToQuat(math.Pi, 0, math.Pi/2, mgl32.XYZ)
-
-		mathglGlobalOutVec3 = quaternion.Rotate(transform)
-	}
-}
-
-func BenchmarkRotateQuaternionGo3D(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := vec3.T{5, 10, 15}
-		quaternion := quaternion.FromEulerAngles(0, math.Pi, math.Pi/2)
-
-		quaternion.RotateVec3(&transform)
-		go3dGlobalOutVec3 = transform
 	}
 }
 
@@ -197,69 +48,6 @@ func BenchmarkRotateQuaternionVkngMath(b *testing.B) {
 		transform.RotateWithQuaternion(&quat)
 
 		vkngMathOutQuat = quat
-	}
-}
-
-func BenchmarkMatrixMultTransformG3n(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		transform := math32.Vector4{X: 5, Y: 10, Z: 15, W: 1}
-
-		var translateMat, rotateMat, scaleMat math32.Matrix4
-		translateMat.MakeTranslation(1, 1, 1)
-		rotateMat.MakeRotationY(1)
-		scaleMat.MakeScale(1.5, 1.5, 1.5)
-
-		var mat math32.Matrix4
-		mat.MultiplyMatrices(
-			&translateMat,
-			&rotateMat,
-		)
-		mat.Multiply(&scaleMat)
-
-		g3nGlobalOutVec4 = *(transform.ApplyMatrix4(&mat))
-	}
-}
-
-func BenchmarkMatrixMultTransformMatGL(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		mat := mgl32.Translate3D(1, 1, 1)
-		mat.Mul4(mgl32.HomogRotate3DY(1))
-		mat.Mul4(mgl32.Scale3D(1.5, 1.5, 1.5))
-
-		transform := mgl32.Vec4{5.0, 10.0, 15.0, 1.0}
-		transform = mat.Mul4x1(transform)
-
-		mathglGlobalOutVec4 = transform
-	}
-}
-
-func BenchmarkMatrixMultTransformGo3D(b *testing.B) {
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		translate := vec3.T{1, 1, 1}
-		scale := vec3.T{1.5, 1.5, 1.5}
-
-		rotate := mat4.Ident
-		rotate.AssignYRotation(1)
-
-		translateMat := mat4.Ident
-		translateMat.Translate(&translate)
-
-		scaleMat := mat4.Ident
-		scaleMat.ScaleVec3(&scale)
-
-		var mat mat4.T
-		mat.AssignMul(&translateMat, &rotate)
-		mat.MultMatrix(&scaleMat)
-		transform := vec4.T{5, 10, 15, 1}
-		mat.TransformVec4(&transform)
-
-		go3dGlobalOutVec4 = transform
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,18 +1,13 @@
 module github.com/vkngwrapper/math
 
-go 1.19
+go 1.25
 
-require (
-	github.com/g3n/engine v0.2.0
-	github.com/go-gl/mathgl v1.0.0
-	github.com/stretchr/testify v1.8.1
-	github.com/ungerik/go3d v0.0.0-20220309204530-55ced4bcb334
-)
+require github.com/stretchr/testify v1.8.1
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/dl v0.0.0-20221207214018-bd7282711064 // indirect
-	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
